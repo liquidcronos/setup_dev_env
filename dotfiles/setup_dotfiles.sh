fd42daa48773164754a8778ff1c805fab67618c2
Jan Baumgärtner <contact@jan-baumgaertner.tech>
2022-01-01 17:12:06 +0100

    added zsh setup

--- dotfiles/setup_dotfiles.sh
+++ dotfiles/setup_dotfiles.sh
@@ -11,7 +11,23 @@ EOF
 ln -sf /home/$USER/setup_dev_env/dotfiles/.i3_config /home/$USER/.config/i3/config
 ln -sf /home/$USER/setup_dev_env/dotfiles/.vimrc /home/$USER/.vimrc
 ln -sf /home/$USER/setup_dev_env/dotfiles/.bashrc /home/$USER/.bashrc
+ln -sf /home/$USER/setup_dev_env/dotfiles/.zshrc /home/$USER/.zshrc
+ln -sf /home/$USER/setup_dev_env/dotfiles/.p10k.zsh /home/$USER/.p10k.zsh
 
+
+#install ZSH dotfile requirements
+cat << EOF
+
+------------------------------------
+Install dotfile requirements for ZSH
+------------------------------------
+
+EOF
+yes | sudo apt install zsh
+chsh -s $(which zsh)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 #install bash dotfile requirements
 cat << EOF
